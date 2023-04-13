d07585c548cbc5ea5713889d5307b0a7c1923b97
123Raihana <105696784+123Raihana@users.noreply.github.com>
2023-04-13 15:52:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+function newPicture(){
+    var buttonDiv = document.getElementsByClassName("divButton")[0];
+    buttonDiv.onmouseover=function(){
+        document.getElementById("btnId").style.display='block';
+
+    }
+    buttonDiv.onmouseout=function(){
+        document.getElementById("btnId").style.display='none';
+        
+    }
+}
+
+function newLicture(){
+    var buttonMiv = document.getElementsByClassName("divButton1")[0];
+    buttonMiv.onmouseover=function(){
+        document.getElementById("idBtn").style.display='block';
+
+    }
+    buttonMiv.onmouseout=function(){
+        document.getElementById("idBtn").style.display='none';
+        
+    }
+}
+
+
+
+
+
+function newBicture(){
+    var buttonBiv = document.getElementsByClassName("divCutton")[0];
+    buttonBiv.onmouseover=function(){
+        document.getElementById("btnId1").style.display='block';
+
+    }
+    buttonBiv.onmouseout=function(){
+        document.getElementById("btnId1").style.display='none';
+        
+    }
+}
+
+
+function newCicture(){
+    var buttonCiv = document.getElementsByClassName("Id")[0];
+    buttonCiv.onmouseover=function(){
+        document.getElementById("idbtn").style.display='block';
+
+    }
+    buttonCiv.onmouseout=function(){
+        document.getElementById("idbtn").style.display='none';
+        
+    }
+}
+
+
+function newDicture(){
+    var buttonDiv = document.getElementsByClassName("Color")[0];
+    buttonDiv.onmouseover=function(){
+        document.getElementById("pinkColor").style.display='block';
+
+    }
+    buttonDiv.onmouseout=function(){
+        document.getElementById("pinkColor").style.display='none';
+        
+    }
+}
+
+
+function newEicture(){
+    var buttonEiv = document.getElementsByClassName("Orange")[0];
+    buttonEiv.onmouseover=function(){
+        document.getElementById("orangeColor").style.display='block';
+
+    }
+    buttonEiv.onmouseout=function(){
+        document.getElementById("orangeColor").style.display='none';
+        
+    }
+}
+
+function newFicture(){
+    var buttonFiv = document.getElementsByClassName("Blue")[0];
+    buttonFiv.onmouseover=function(){
+        document.getElementById("blueColor").style.display='block';
+
+    }
+    buttonFiv.onmouseout=function(){
+        document.getElementById("blueColor").style.display='none';
+        
+    }
+}
+
+
+function newGicture(){
+    var buttonGiv = document.getElementsByClassName("Violet")[0];
+    buttonGiv.onmouseover=function(){
+        document.getElementById("violetColor").style.display='block';
+
+    }
+    buttonGiv.onmouseout=function(){
+        document.getElementById("violetColor").style.display='none';
+        
+    }
+}
+
+
+function newHicture(){
+    var buttonHiv = document.getElementsByClassName("Black")[0];
+    buttonHiv.onmouseover=function(){
+        document.getElementById("blackColor").style.display='block';
+
+    }
+    buttonHiv.onmouseout=function(){
+        document.getElementById("blackColor").style.display='none';
+        
+    }
+}
+
+function alertMessage(){
+    alert("Please add to cart...Have a nice shopping :)");
+}
